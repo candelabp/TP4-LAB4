57e26cc1f61682fb0d062c649a096c2314f1246d
flopigimenez <fgimenezmorcos@gmail.com>
2025-05-16 08:30:04 -0300

    boton admin

--- tp4-lab/src/components/Navbar.tsx
+++ tp4-lab/src/components/Navbar.tsx
@@ -51,7 +51,11 @@ export const Navbar = () => {
             <li><button className="navbar-link-btn" onClick={() => navigate('/home')}>Inicio</button></li>
             <li><ScrollLink smooth={true} duration={100} to="donde-estamos"><span className="navbar-link-btn">Donde estamos</span></ScrollLink></li>
             <li><ScrollLink smooth={true} duration={100} to="productos"><span className="navbar-link-btn">Productos</span></ScrollLink></li>
-            <li><button className="navbar-link-btn" onClick={() => navigate('/tabla')}>Admin</button></li>
+            {usuarioLogueado && usuarioLogueado.rol === Rol.ADMIN && (
+              <li>
+                <button className="navbar-link-btn" onClick={() => navigate('/tabla')}>Admin</button>
+              </li>
+            )}
             <li><button className="navbar-link-btn" onClick={() => navigate('/pedidos')}>Pedidos</button></li>
             <li><button className="navbar-link-btn" onClick={() => {
               if (!usuarioLogueado) {
